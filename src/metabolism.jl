aa3dd1844b0293f221f8e62e780af18824e87e56
John Eargle <jeargle@gmail.com>
2025-08-02 15:16:52 -0500

    #67 added stub for get_connected_submetabolisms()

--- src/metabolism.jl
+++ src/metabolism.jl
@@ -27,25 +27,30 @@ struct Metabolism
             for reactant in reaction.reactants
                 push!(nodes, reactant)
                 push!(edges, (reactant, reaction))
+
                 if haskey(children, reactant)
                     push!(children[reactant], reaction)
                 else
                     children[reactant] = Set([reaction])
                 end
+
                 if haskey(parents, reaction)
                     push!(parents[reaction], reactant)
                 else
                     parents[reaction] = Set([reactant])
                 end
             end
+
             for product in reaction.products
                 push!(nodes, product)
                 push!(edges, (reaction, product))
+
                 if haskey(children, reaction)
                     push!(children[reaction], product)
                 else
                     children[reaction] = Set([product])
                 end
+
                 if haskey(parents, product)
                     push!(parents[product], reaction)
                 else
@@ -55,6 +60,54 @@ struct Metabolism
         end
         new(nodes, edges, children, parents)
     end
+end
+
+
+"""
+    get_source_nodes(metabolism)
+
+Retrieve a set of nodes that have no parents.
+
+# Arguments
+- `metabolism`: Metabolism
+
+# Returns
+- `Set{Union{Molecule, Reaction}}`:
+"""
+function get_source_nodes(metabolism)
+    source_nodes = Set{Union{Molecule, Reaction}}()
+
+    for node in metabolism.nodes
+        if !haskey(metabolism.parents, node)
+            push!(source_nodes, node)
+        end
+    end
+
+    return source_nodes
+end
+
+
+"""
+    get_terminal_nodes(metabolism)
+
+Retrieve a set of nodes that have no children.
+
+# Arguments
+- `metabolism`: Metabolism
+
+# Returns
+- `Set{Union{Molecule, Reaction}}`:
+"""
+function get_terminal_nodes(metabolism)
+    terminal_nodes = Set{Union{Molecule, Reaction}}()
+
+    for node in metabolism.nodes
+        if !haskey(metabolism.children, node)
+            push!(terminal_nodes, node)
+        end
+    end
+
+    return terminal_nodes
 end
 
 
@@ -79,4 +132,50 @@ function merge_metabolisms(metabolism1, metabolism2)
     # merged_metabolism = Metabolism(molecules, reactions, reactant_edges, product_edges)
 
     # return merged_metabolism
+end
+
+
+"""
+    get_connected_metabolism(metabolism)
+
+Create a Metabolism from the connected subgraph of an input Metabolism and one
+of its nodes.
+
+# Arguments
+- `metabolism`: Metabolism
+- `node`: Union{Molecule, Reaction}
+
+# Returns
+- `Metabolism`:
+"""
+function get_connected_submetabolism(metabolism, node)
+    # Collect nodes and edges into dictionaries for fast lookup.
+
+    next_nodes = Set{Union{Molecule, Reaction}}()
+    union!(next_nodes, metabolism.children[node])
+    union!(next_nodes, metabolism.parents[node])
+    # submetabolism = Metabolism(molecules, reactions, reactant_edges, product_edges)
+
+    # return submetabolism
+end
+
+
+"""
+    get_connected_metabolisms(metabolism)
+
+Create a list of Metabolisms from the connected subgraphs of an input
+Metabolism.
+
+# Arguments
+- `metabolism`: Metabolism
+
+# Returns
+- `Array{Metabolism, 1}`:
+"""
+function get_connected_submetabolisms(metabolism)
+    # Collect nodes and edges into dictionaries for fast lookup.
+
+    # submetabolisms = Metabolism(molecules, reactions, reactant_edges, product_edges)
+
+    # return submetabolisms
 end
